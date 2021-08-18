6de39abdf0a650b38869039909775f971581fdab
Grenko92 <pochtarenko1992@gmail.com>
2021-08-18 12:04:59 +0300

    18.08.21-1

--- script/script.js
+++ script/script.js
@@ -15,3 +15,24 @@ function closePopupVideo() {
 openBtnPopupVideo.addEventListener("click", openPopupVideo);
 closeBtnPopupVideo.addEventListener("click", closePopupVideo);
 closeAreaPopupVideo.addEventListener("click", closePopupVideo);
+
+// TEST
+
+const wrap = document.querySelector(".wrap");
+// let clone1 = document.querySelector(".clone");
+// let clone2 = clone1.cloneNode(true);
+// let clone3 = clone1.cloneNode(true);
+// let a = 0;
+
+// function clone() {
+//     a++;
+//     console.log(a);
+//     wrap.prepend(clone2);
+//     wrap.append(clone2);
+//     wrap.prepend(clone3);
+//     wrap.append(clone3);
+// }
+
+// wrap.addEventListener("animationiteration", clone);
+
+// console.log(a);
