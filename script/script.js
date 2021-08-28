861d52b02e33315ad51bf5bc0b9695d23fe53ac5
Grenko92 <pochtarenko1992@gmail.com>
2021-08-28 18:01:33 +0300

    28.08.21

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+    // Показать-убрать попап видео
+
 const openBtnPopupVideo = document.querySelector(".start-screen__btn-video");
 const closeBtnPopupVideo = document.querySelector(".start-screen__popup-btn-close");
 const closeAreaPopupVideo = document.querySelector(".start-screen__popup-bg-video");
@@ -16,18 +18,71 @@ openBtnPopupVideo.addEventListener("click", openPopupVideo);
 closeBtnPopupVideo.addEventListener("click", closePopupVideo);
 closeAreaPopupVideo.addEventListener("click", closePopupVideo);
 
-// TEST
+    // Анимация появления блоков
 
+let animationBlocks = document.querySelectorAll(".animation")
 
-let testDiv = document.querySelector(".test");
-let posDiv = testDiv.getBoundingClientRect();
-console.log(window.innerHeight);
+function animation () {
+    for (let elem of animationBlocks) {
+        let elemPos = elem.getBoundingClientRect().y < window.innerHeight;
 
-function testFunc() {
-    console.log(window.innerHeight);
+        if (elemPos) {
+            elem.classList.add("animation--active");
+        }
+    }
 }
 
-window.addEventListener("scroll", testFunc);
+            // function animation () {
+            //     for (let elem of animationBlocks) {
+            //         let elemPos = Math.round(elem.getBoundingClientRect().y - window.innerHeight);
 
+            //         if (elemPos < 40) {
+            //             elem.classList.add("animation--active");
+            //         }
+            //     }
+            // }
 
-// testDiv.getBoundingClientRect()
+document.addEventListener("DOMContentLoaded", animation);
+window.addEventListener("scroll", animation);
+
+    //  Кнопка Связаться с нами
+
+const recallLink = document.querySelector(".recall__link");
+const recallText = document.querySelector(".recall__text");
+
+function animationRecall() {
+    elem.classList.add("animationRecall--active");
+}
+
+function animationRecallIn () {
+    recallText.classList.add("animation-recall--in");
+    recallText.classList.remove("animation-recall--out");
+}
+
+function animationRecallOut () {
+    recallText.classList.add("animation-recall--out");
+    recallText.classList.remove("animation-recall--in");
+}
+
+recallLink.addEventListener("mouseenter", animationRecallIn);
+recallLink.addEventListener("mouseleave", animationRecallOut);
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 60,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-right',
+      prevEl: '.swiper-button-left',
+    },
+
+    pagination: {
+        el: '.swiper-pagination-progressbar',
+        type: 'progressbar',
+      },
+  });
+
